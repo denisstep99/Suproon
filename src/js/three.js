5720a6c10bd6bb95f5af8ad2ce032306a840ffc2
Denis <denisstep99@gmail.com>
2020-06-03 17:28:01 +0300

    controll buttons

--- src/js/three.js
+++ src/js/three.js
@@ -1,11 +1,19 @@
 const THREE = require('three');
 const GEOMETRY = {
-    'BOX': new THREE.BoxGeometry(0.2, 0.2, 0.2),
+    'BOX': new THREE.BoxGeometry(0.2, 0.3, 0.2),
     'CONE': new THREE.ConeGeometry( 0.2, 0.2, 3 ),
     'CYLINDER': new THREE.CylinderGeometry( 0.15, 0.15, 0.2, 32 )};
 
 let camera, scene, renderer, additionalRenderer;
 let geometry, material, mesh;
+
+const buttons = {
+    isRightButtonPressed: false,
+    isLeftButtonPressed: false,
+    isScalePlusButtonPressed: false,
+    isScaleMinusButtonPressed: false
+}
+
 
 function init(model, additionalModel) {
     camera = new THREE.PerspectiveCamera(70, model.clientWidth / model.clientHeight, 0.01, 10);
@@ -30,8 +38,9 @@ function init(model, additionalModel) {
 
 
     window.addEventListener('resize', onWindowResize.bind(null, model), false);
-    document.querySelectorAll('.btn-bar__tab-radio')
+    document.querySelectorAll('.btn-bar_side_left .btn-bar__tab-radio')
         .forEach(e => e.addEventListener('change', changeGeometry));
+    checkControlButtons();
 }
 
 function changeGeometry({target}) {
@@ -45,11 +54,66 @@ function changeGeometry({target}) {
 function animate() {
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    if (buttons.isRightButtonPressed) {
+        rotateRight();
+    }
+    if (buttons.isLeftButtonPressed) {
+        rotateLeft();
+    }
+    if (buttons.isScaleMinusButtonPressed) {
+        scaleMinus();
+    }
+    if (buttons.isScalePlusButtonPressed) {
+        scalePlus();
+    }
 
     renderer.render(scene, camera);
     additionalRenderer.render(scene, camera);
+}
+
+function scalePlus() {
+    camera.fov--;
+    camera.updateProjectionMatrix();
+}
+
+function scaleMinus() {
+    camera.fov++;
+    camera.updateProjectionMatrix();
+}
+
+function rotateRight() {
+    mesh.rotation.y += 0.01;
+}
+
+function rotateLeft() {
+    mesh.rotation.y -= 0.01;
+}
+
+function checkControlButtons() {
+    document.querySelector(".btn-bar__label_type_rotation-right").addEventListener('mousedown', () => {
+        buttons.isRightButtonPressed = true;
+    });
+    document.querySelector(".btn-bar__label_type_rotation-right").addEventListener('mouseup', () => {
+        buttons.isRightButtonPressed = false;
+    });
+    document.querySelector(".btn-bar__label_type_rotation-left").addEventListener('mousedown', () => {
+        buttons.isLeftButtonPressed = true;
+    });
+    document.querySelector(".btn-bar__label_type_rotation-left").addEventListener('mouseup', () => {
+        buttons.isLeftButtonPressed = false;
+    });
+    document.querySelector(".btn-bar__label_type_scale-plus").addEventListener('mousedown', () => {
+        buttons.isScalePlusButtonPressed = true;
+    });
+    document.querySelector(".btn-bar__label_type_scale-plus").addEventListener('mouseup', () => {
+        buttons.isScalePlusButtonPressed = false;
+    });
+    document.querySelector(".btn-bar__label_type_scale-minus").addEventListener('mousedown', () => {
+        buttons.isScaleMinusButtonPressed = true;
+    });
+    document.querySelector(".btn-bar__label_type_scale-minus").addEventListener('mouseup', () => {
+        buttons.isScaleMinusButtonPressed = false;
+    });
 }
 
 // Небольшой баг, если изменить размер с камерой на главном, а потом переключить, модель обрежется.
